55aea816059126b2a91082a2cc7a2aeec7c620a3
AntoineYu <yzq356350@gmail.com>
2020-03-27 22:12:18 +0100

    Creation

---
+++ src/test/groovy/gl51/movie/service/impl/MovieRegistryImplTest.groovy
@@ -0,0 +1,28 @@
+package gl51.movie.service.impl
+
+import io.micronaut.test.annotation.MicronautTest
+
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class MovieRegistryImplTest extends Specification{
+    @Inject MovieRegistryImpl registry
+
+    void "injection should work"() {
+        expect:
+            registry != null
+    }
+
+    void "favorites should be empty"() {
+        expect:
+        registry.listFavorites() == []
+    }
+
+    void "adding a favorites should fill in the database"() {
+        when:
+            registry.addMovieToFavorites("aaaa")
+        then:
+            registry.listFavorites().size() == 1
+    }
+}
